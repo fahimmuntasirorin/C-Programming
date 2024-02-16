0702932301daac11b448a420a873721b66dc9254
developerorin2002 <developerorin2002@gmail.com>
2024-02-16 23:38:51 +0600

    remove py file

---
+++ input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+
+    int number1;
+    scanf("%d",&number1);
+    printf("%d",number1);
+    return 0;
+};
+
+// & => here is & is a sign of Address of 
+
--- variable.c
+++ variable.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
-
-
 int main () {
     int rahim = 10;
     printf("%d",rahim);
     float karim = 20.30;
     printf("%0.2f",karim);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
