449666238ba7f20f6b5690d2554ef5fabe252055
developerorin2002 <developerorin2002@gmail.com>
2024-02-16 22:27:36 +0600

    variable added

--- hello.c
+++ hello.c
@@ -3,6 +3,10 @@ int main () {
     printf("orin\nfahim\t muntasir orin");
     return 0;
 };
+//<stdio.h> => standerd input and output header file 
+// printf => built in function used for show output
+// \n => for new line 
+// \t => for new tab
 
 
 
---
+++ variable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main () {
+    int rahim = 10;
+    printf("%d",rahim);
+    float karim = 20.30;
+    printf("%0.2f",karim);
+};
+
+
+
+
+
+
+
+
