ca773b3cde4a1b32dded900df4a0778c10d858e5
developerorin2002 <developerorin2002@gmail.com>
2024-02-17 21:42:46 +0600

    limitation done

---
+++ limitation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    // int my_number;
+    // my_number = 100000034500;
+    // printf("%d",my_number);
+    float my_number2 = 23432;
+    printf("%f",my_number2);
+    return 0;
+};
+
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+  // int my_number;
+    // my_number = 100000034500;
+    // printf("%d",my_number);
